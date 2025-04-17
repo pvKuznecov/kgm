8cc52f704eb40c0faf493ee0ff3ec700e644ca41
Kuznetsov Pavel <p.v.kuznecov@yandex.ru>
2025-04-17 16:22:46 +0700

    start

---
+++ src/views/CounterView/CounterView.vue
@@ -0,0 +1,13 @@
+<template src="./CounterView.html"></template>
+<style src="./CounterView.css"></style>
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+    name: 'CounterView',
+    // components: {
+    //     HelloWorld
+    // }
+}
+</script>
---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,14 @@
+<template src="./HomeView.html"></template>
+<style src="./HomeView.css"></style>
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+
+export default {
+    name: 'HomeView',
+    // components: {
+    //     HelloWorld
+    // }
+}
+</script>
